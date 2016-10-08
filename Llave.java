5eac3bc5ccb764821a830f64e351f6c2ac4d8167
ARebollo <muaddib58@gmail.com>
2016-10-08 15:03:27 +0200

    Add files via upload

---
+++ Llave.java
@@ -0,0 +1,139 @@
+package Estructuras;
+
+/**
+* Declaracion de la clase Llave
+* @author
+*   <b> Antonio Rebollo Guerra, Carlos Salguero Sanchez </b><br>
+*   <b> Asignatura Desarrollo de Programas</b><br>
+*   <b> Curso 15/16 </b>
+*/
+public class Llave implements Comparable<Llave> {
+	
+	int id;
+	
+	/**
+   	 * Constructor default de la clase Llave
+   	 */
+	Llave (){
+		
+		id = -1;
+		
+	}
+	
+	/**
+   	 * Constructor parametrizado de la clase Llave
+   	 * 
+   	 * @param id Id de la llave
+   	 * 
+   	 */
+	public Llave (int id){
+		
+		this.id = id;
+		
+	}
+
+	/**
+   	 * Constructor por copia de la clase Llave
+   	 * 
+   	 * @param Llave Objeto de la clase Llave
+   	 * 
+   	 */
+	Llave (Llave Llave) {
+		
+		this.id = Llave.getId();
+		
+	}
+	
+	// Getters & Setters
+	
+	/**
+   	 * Obtiene el atributo Id de la clase Llave
+   	 * 
+   	 * @return Id de la llave
+   	 * 
+   	 */
+	public int getId (){
+		
+		return id;
+		
+	}
+	
+	/**
+   	 * Cambia el valor del atributo Id de la clase Llave
+   	 * 
+   	 * @param id Nuevo valor entero
+   	 * 
+   	 */
+	public void setId (int id){
+		
+		this.id = id;
+		
+	}
+	
+	/**
+   	 * Compara el valor de id entre dos Llaves
+   	 * 
+   	 * @param mComp Objeto de clase Llave
+   	 * 
+   	 * @return -1 si la id de la clase es menor <br> 0 si la id de la clase es la misma <br> 1 si la id de la clase es mayor
+   	 * 
+   	 */
+	@Override
+	public int compareTo(Llave mComp){
+		if (this.id < mComp.getId())
+			return -1;
+		if (this.id == mComp.getId())
+			return 0;
+		else
+			return 1;
+	}
+	
+	
+	
+	/**
+   	 * Muestra la informacion de la clase Llave
+   	 * 
+   	 */
+	@Override
+	public String toString(){
+		
+		String aux = "" + id;
+		return aux;
+		
+	}
+
+	@Override
+	/**
+   	 * Devuelve el hashCode de un Llave
+   	 * 
+   	 * @return Entero con el valor del hashCode
+   	 * 
+   	 */
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * (result + id);
+		return result;
+	}
+	
+	/**
+   	 * Comprueba si dos Llave son iguales
+   	 * 
+   	 * @return True : si ambos Llave son iguales <br> False : si los Llave son distintos
+   	 * 
+   	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Llave other = (Llave) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+}
---
+++ Puerta.java
@@ -0,0 +1,355 @@
+package DEV;
+
+
+import java.util.LinkedList;
+import java.util.List;
+
+import Estructuras.Llave;
+import Estructuras.abb;
+
+/**
+* Declaracion de la clase Puerta
+* @author
+*   <b> Antonio Rebollo Guerra, Carlos Salguero Sanchez </b><br>
+*   <b> Asignatura Desarrollo de Programas</b><br>
+*   <b> Curso 15/16 </b>
+*/
+public class Puerta {
+
+	private abb<Llave> Probados;
+	private abb<Llave> Combinacion;
+	private boolean estado; // True abierta, False cerrada
+	private int profundidad;
+	private Llave[] vectorCfg;
+	
+	/**
+   	 * Constructor default de la clase Puerta
+   	 * 
+   	 */
+	public Puerta(){
+		
+		estado = false;
+		Probados = new abb<Llave>();
+		Combinacion = new abb<Llave>();
+		profundidad = 0;
+		vectorCfg = new Llave[200];	
+		
+	}
+	
+	/**
+   	 * Constructor parametrizado de la clase Puerta
+   	 * 
+   	 * @param vector Vector de Llaves
+   	 * @param constante Constante de profundidad para la cerradura de la puerta
+   	 * 
+   	 */
+	Puerta(Llave [] vector, int constante){ // Constructor parametrizado, para configurar la combinacion pertinente
+		
+		estado = false;
+		Probados = new abb<Llave>();
+		Combinacion = new abb<Llave>();
+		profundidad = constante;
+		vectorCfg = vector;	
+		
+		configurarCombinacionCfg(0, vectorCfg.length-1);
+	}
+	
+	/**
+   	 * Crea la combinacion de la puerta a traves de una lista de Llaves
+   	 * 
+   	 */
+	public void setCombinacionList(List<Llave> list){
+				
+		vectorCfg = new Llave[list.size()];
+		
+		for (int i = 0; i < list.size(); i++){
+			
+			vectorCfg[i] = list.get(i);
+		}
+		
+		configurarCombinacionCfg(0, vectorCfg.length-1);
+	}
+	
+	/**
+   	 * Transforma una linkedlist de Llaves a un vector de Llaves
+   	 * 
+   	 */
+	public void setVectorCfgLinkedList(LinkedList<Llave> list){
+		
+		vectorCfg = new Llave[list.size()];
+		
+		for (int i = 0; i < list.size(); i++){
+			
+			vectorCfg[i] = list.get(i);
+		}
+		
+	}
+	
+	/**
+   	 * Muestra el vector de configuracion por pantalla
+   	 * 
+   	 */
+	public void mostrarVectorCfg(){
+		
+		for (int i = 0;i<vectorCfg.length;i++){
+			
+			System.out.print(" "+ vectorCfg[i]);
+		}
+		System.out.println();
+	}
+	
+	/**
+   	 * Metodo para devolver el vector de configuracion que tiene la Puerta en forma de String
+   	 * 
+   	 * @return Cadena con el vector de configuracion de la Puerta
+   	 * 
+   	 */
+	public String getVectorCfgString(){
+		
+		String a ="";
+		
+		for (int i = 0;i<vectorCfg.length;i++){
+
+			a += vectorCfg[i] + " ";
+		}
+		
+	  return a;
+	}
+
+	
+	/**
+   	 * Cierra la puerta en caso de que este abierta, si no, la reinicia a su estado original
+   	 * 
+   	 */
+	public void cerrarPuerta (){
+		
+		if (this.estado == false)  // Si ya estaba cerrada reiniciarla
+		{		 
+		 this.Combinacion = new abb<Llave>();
+		 configurarCombinacionCfg(0, vectorCfg.length-1);
+		 //TODO Quizás haya que reiniciar los probados? No creo, pero es posible. 
+		 //También quitar los mensajes de texto.
+		 System.out.println();
+		 System.out.println("Reiniciando puerta...");
+		}
+		else  // Si estaba abierta, cerrarla
+		{
+		 this.estado = false;
+		 System.out.println();
+		 System.out.println("Cerrando puerta...");
+		}
+		
+	}
+
+	/**
+   	 * Crea la combinacion de la puerta a partir de un vector de Llaves, siguiendo el algoritmo especificado en la entrega <br>
+   	 * Para ello inserta el valor medio y se llama recursivamente primero para la mitad derecha del vector y posteriormente para la izquierda, 
+   	 * usando un vector auxiliar como parametro de entrada.
+   	 * 
+   	 * @param vector Vector de Llave
+   	 * 
+   	 */
+	@Deprecated
+	private void configurarCombinacion (Llave [] vector, int izq, int der){
+	       
+        int mit = izq + (der - izq)/2;
+         //Insercion valor intermedio
+        Combinacion.insertar(vector[mit]);
+        if (izq<der && mit != 0)
+        { 	        	
+         //Copia subvector derecho y llamada recursiva
+         configurarCombinacion(vector, mit+1, der);
+         
+         //Copia subvector izquierdo y llamada recursiva
+         configurarCombinacion(vector, izq, mit-1);
+        }
+       
+    }
+	
+	/**
+   	 * Crea la combinacion de la puerta a partir del vector de Llaves vectorCfg, siguiendo el algoritmo especificado en la entrega <br>
+   	 * Para ello inserta el valor medio y se llama recursivamente primero para la mitad derecha del vector y posteriormente para la izquierda.
+   	 * 
+   	 * @param izq Limite izquierdo del subvector en cada llamada 
+   	 * 
+   	 * @param der Limite derecho del subvector en cada llamada
+   	 * 
+   	 */
+	private void configurarCombinacionCfg (int izq, int der){
+	       
+        int mit = izq + (der - izq)/2;
+         //Insercion valor intermedio
+        Combinacion.insertar(vectorCfg[mit]);
+        if (izq<der && mit != 0)
+        { 	        	
+         //Copia subvector derecho y llamada recursiva
+         configurarCombinacionCfg(mit+1, der);
+         
+         //Copia subvector izquierdo y llamada recursiva
+         configurarCombinacionCfg(izq, mit-1);
+        }
+       
+    }
+	
+	/**
+   	 * Metodo para probar una unica Llave en la cerradura
+   	 * 
+   	 * @param Llave Objeto de la clase Llave
+   	 * 
+   	 */
+	public void probarLlave (Llave Llave){  
+			
+			if (Llave != null)
+			{
+				if (Probados.existe(Llave) == true)
+				{
+					System.out.println("Esta llave ya se ha probado.");
+					//TODO Quizás quitar mensajes de texto?
+				}
+				else
+				{
+					Probados.insertar(Llave);
+				}
+				if (Combinacion.existe(Llave) == true)
+				{
+					Combinacion.eliminar(Llave);
+				}
+		
+				if (Combinacion.altura() < profundidad && Combinacion.numHojas() <= (Combinacion.numNodos() - Combinacion.numHojas()) )
+				{
+					estado = true;
+					System.out.println("Abriendo puerta...");
+				}
+			}
+
+	}
+	
+	// Getters & Setters
+	
+	/**
+   	 * Obtiene el arbol Probados de la clase Puerta
+   	 * 
+   	 * @return Arbol de los Llave probados
+   	 * 
+   	 */
+	public abb<Llave> getProbados() {
+		
+		return Probados;
+	}
+	
+	/**
+   	 * Cambia el arbol Probados de la clase Puerta
+   	 * 
+   	 * @param probados Nuevo arbol de Llave
+   	 * 
+   	 */
+	public void setProbados(abb<Llave> probados) {
+		
+		Probados = probados;
+	}
+	
+	/**
+   	 * Obtiene el arbol Combinacion de la clase Puerta
+   	 * 
+   	 * @return Arbol de la combinacion de la Puerta
+   	 * 
+   	 */
+	public abb<Llave> getCombinacion() {
+		
+		return Combinacion;
+	}
+	
+	/**
+   	 * Cambia el arbol Combinacion de la clase Puerta
+   	 * 
+   	 * @param combinacion Nuevo arbol de Llave
+   	 * 
+   	 */
+	public void setCombinacion(abb<Llave> combinacion) {
+		
+		Combinacion = combinacion;
+	}
+	
+	/**
+   	 * Indica si la Puerta esta abierta o cerrada
+   	 * 
+   	 * @return True : si la Puerta esta abierta <br> False : si la Puerta esta cerrada
+   	 * 
+   	 */
+	public boolean isEstado() {
+		
+		return estado;
+	}
+	
+	/**
+   	 * Cambia el valor del booleano Estado de la clase Estacion
+   	 * 
+   	 * @param estado Nuevo valor (True || False)
+   	 * 
+   	 */
+	public void setEstado(boolean estado) {
+		
+		this.estado = estado;
+	}
+	
+	/**
+   	 * Obtiene el atributo Profundidad de la clase Puerta
+   	 * 
+   	 * @return Entero con la profundidad de la Puerta
+   	 * 
+   	 */
+	public int getProfundidad() {
+		
+		return profundidad;
+	}
+	
+	/**
+   	 * Cambia el valor del atributo Profundidad de la clase Puerta
+   	 * 
+   	 * @param profundidad Nuevo valor entero
+   	 * 
+   	 */
+	public void setProfundidad(int profundidad) {
+		
+		this.profundidad = profundidad;
+	}
+	
+	/**
+   	 * Obtiene el vector de configuracion de la clase Puerta
+   	 * 
+   	 * @return Vector con los Llaveclorianos que se van a usar en la configuracion de la combinacion
+   	 * 
+   	 */
+	public Llave[] getVectorCfg() {
+		
+		return vectorCfg;
+	}
+	
+	/**
+   	 * Cambia el vector de configuracion de la clase Puerta
+   	 * 
+   	 * @param vectorCfg Nuevo vector de Llave
+   	 * 
+   	 */
+	public void setVectorCfg(Llave[] vectorCfg) {
+		
+		this.vectorCfg = vectorCfg;
+	}
+	
+	public static void main(String[] args){
+		
+		Llave[] vect = new Llave[15];
+		for (int i = 1; i<=15;i++)
+		{
+			vect[i-1] = new Llave(2*i - 1);
+		}
+		Puerta p = new Puerta(vect, 4);
+		p.mostrarVectorCfg();
+		System.out.println(p.getCombinacion().arbolAString());
+		p.getCombinacion().anchura();
+		p.probarLlave(new Llave(5));
+		p.getCombinacion().anchura();
+	}
+	
+}
+
