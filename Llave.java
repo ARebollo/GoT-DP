95d31d6a784155051f445a45e354d2b54634146d
ARebollo <muaddib58@gmail.com>
2016-10-16 23:51:07 +0200

    Update Llave.java

--- Llave.java
+++ Llave.java
@@ -108,10 +108,7 @@ public class Llave implements Comparable<Llave> {
    	 * 
    	 */
 	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * (result + id);
-		return result;
+		return 31 * (1+id);
 	}
 	
 	/**
